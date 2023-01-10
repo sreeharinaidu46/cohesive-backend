a83a892f46fc9bcd082b9e62535a2c3647c310ac
Sreeharinaidu <sreeharinaidurangani@gmail.com>
2023-01-10 13:25:28 +0530

    new

--- server.js
+++ server.js
@@ -14,15 +14,7 @@ mongoose.connect(process.env.MONGODB_URI || MONGO_URI, {
 mongoose.connection.on("connected", () => {
     console.log("mongodb is connected");
 });
-if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static('client/build'))
 
-    app.get('*', (req, res) => {
-
-        res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
-
-    })
-}
 
 const app = express();
 app.use(express.json())
@@ -30,9 +22,7 @@ app.use(cors());
 app.use('/public', express.static('public'));
 app.use('/user', userRoutes)
 app.use('/user', transactionRoutes)
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static('client/build'));
-}
+
 const server = app.listen(port, () => {
     console.log('Connected to port ' + port)
 })
